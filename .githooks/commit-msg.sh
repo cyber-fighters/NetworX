a0fee9ceb0b3f199d6a9d8364bae668429be2a9a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-21 10:16:33 +0200

    Add git hook

---
+++ .githooks/commit-msg.sh
@@ -0,0 +1,12 @@
+#!C:/Program\ Files/Git/usr/bin/sh.exe
+
+message_file=$1
+commit_msg=$(cat "${1:?Missing commit message file}")
+
+if [[ $commit_msg =~ ^[A-Z0-9]+.*$ ]]; then
+  echo "[POLICY] Capital letter test passed"
+  exit 0
+else
+  echo "[POLICY] Please start your commit message with a capital letter"
+  exit 1
+fi
