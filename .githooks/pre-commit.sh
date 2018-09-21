045e695ef416b48e93ace37d1523f86e340ee8f6
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-21 11:37:56 +0200

    Add notebook content hook

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Fail if trying to commit executed jupyter notebook
- diff=$(git diff --cached)
- if [[ $diff =~ ^.*"execution_count":\ [0-9]+,.*$ ]]; then
+ diff=$(git diff --cached | grep -e '^[\+]\s.*' | grep -e '\"execution_count\": [0-9][0-9]*')
+
+ if [[ -z $diff ]]; then
+   echo "[CONTENT] Jupyter notebook test passed"
+ else
    echo "[POLICY] Error: Please clear the output of the jupyter notebook before committing"
    exit 1
- else
-   echo "[CONTENT] Jupyter notebook test passed"
  fi
