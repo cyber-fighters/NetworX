049df6eca0cc70d71108cb04d2e2fdba4c81dd00
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-21 11:37:33 +0200

    Configure hooks

--- .githooks/commit-msg.sh
+++ .githooks/commit-msg.sh
@@ -7,15 +7,15 @@ commit_msg=$(head -1 "${1:?Missing commit message file}")
 if [[ $commit_msg =~ ^[A-Z]+.*$ ]]; then
   echo "[POLICY] Capital letter test passed"
 else
-  echo "[POLICY] Please start your commit message with a capital letter"
+  echo "[POLICY] Error: Please start your commit message with a capital letter"
   exit 1
 fi
 
 # enforce maxium length
-MAX_LEN=50
+MAX_LEN=75
 if [[ ${#commit_msg} -lt $MAX_LEN ]]; then
   echo "[POLICY] Maximum message length test passed"
 else
-  echo "[POLICY] You commit message is too long (${#commit_msg} > $MAX_LEN)"
+  echo "[POLICY] Error: Your commit message is too long (${#commit_msg} > $MAX_LEN)"
   exit 1
 fi
--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-echo Hi!
-exit 1
+# Fail if trying to commit executed jupyter notebook
+ diff=$(git diff --cached)
+ if [[ $diff =~ ^.*"execution_count":\ [0-9]+,.*$ ]]; then
+   echo "[POLICY] Error: Please clear the output of the jupyter notebook before committing"
+   exit 1
+ else
+   echo "[CONTENT] Jupyter notebook test passed"
+ fi
