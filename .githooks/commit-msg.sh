87884ff2d25e64a60f6e2fe77973c6a9b6c3fe4a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-09-21 11:03:25 +0200

    Simplify hook

--- .githooks/commit-msg.sh
+++ .githooks/commit-msg.sh
@@ -5,7 +5,6 @@ commit_msg=$(cat "${1:?Missing commit message file}")
 
 if [[ $commit_msg =~ ^[A-Z0-9]+.*$ ]]; then
   echo "[POLICY] Capital letter test passed"
-  exit 0
 else
   echo "[POLICY] Please start your commit message with a capital letter"
   exit 1
